ceafa8d42dd6aae31583b7de330f9b4036ccf9f0
Ihor <tsake.love@gmail.com>
2020-04-11 13:14:22 +0300

    initial
    created basic settings

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+const Koa = require('koa')
+const app = new Koa()
+const chalk = require('chalk')
+const debug = require('debug')('index')
+const morgan = require('koa-morgan')
+const path = require('path')
+const rfs = require('rotating-file-stream')
+const accessLogStream = rfs.createStream('access.log', {
+  interval: '1d', // rotate daily
+  path: path.join(__dirname, 'log')
+})
+app.use(morgan('combined', { stream: accessLogStream }))
+
+app.use(function * () {
+  this.body = 'Hello world!'
+})
+
+app.listen(3000, function () {
+  debug(`listening on port ${chalk.green('3000')}`)
+})
