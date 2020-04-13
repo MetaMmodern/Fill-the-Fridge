4c8c4f285c3fbc9dd4319b1eecd6af041db2c197
Ihor <tsake.love@gmail.com>
2020-04-13 19:12:43 +0300

    add basic router
    open access to the main page what information to be filled

--- index.js
+++ index.js
@@ -6,16 +6,34 @@ const debug = require('debug')('index')
 const morgan = require('koa-morgan')
 const path = require('path')
 const rfs = require('rotating-file-stream')
+const render = require('koa-ejs')
+const KoaRouter = require('koa-router')
 const port = process.env.PORT || 3000
 const accessLogStream = rfs.createStream('access.log', {
   interval: '1d', // rotate daily
   path: path.join(__dirname, 'log')
 })
+const router = new KoaRouter()
 app.use(morgan('tiny', { stream: accessLogStream }))
-
-app.use(function * () {
-  this.body = 'Hello world!'
+render(app, {
+  root: path.join(__dirname, 'view'),
+  layout: false,
+  viewExt: 'html',
+  cache: false,
+  debug: true
 })
+router.get('/', (ctx) => {
+  const arrayOfAttribute = []
+  arrayOfAttribute.push({
+    text: 'Awesome text'
+  })
+  const mainTitle = 'Fill-the-fridge'
+  return ctx.render('index', {
+    attributes: arrayOfAttribute,
+    title: mainTitle
+  })
+})
+app.use(router.routes())
 app.listen(port, function () {
   debug(`listening on port ${chalk.green('3000')}`)
 })
