cb9a412d908ac920841968d12fa82214bc5b9e22
metamodern <shabanovbunya@gmail.com>
2020-06-02 10:21:37 +0300

    integrate map
    also added some comments of sources

--- public/assets/js/popupload.js
+++ public/assets/js/popupload.js
@@ -1,8 +1,56 @@
+// eslint-disable-next-line import/extensions
+import initMap from './map.js';
+
+function initializeMap(mapButton, storesForMap) {
+  mapButton.addEventListener('click', e => {
+    e.preventDefault();
+    // commented below is check for mobile device, was got from https://redstapler.co/detect-mobile-device-with-javascript/
+
+    // if (
+    //   navigator.userAgent.match(/Android/i) ||
+    //   navigator.userAgent.match(/iPhone/i) ||
+    //   navigator.userAgent.match(/iPad/i) ||
+    //   navigator.userAgent.match(/iPod/i)
+    // ) {
+
+    // } else {
+
+    // }
+    const wholemodalReciep = document.getElementById('wholemodalReciep');
+    const reciepContent = document.getElementById('reciepContent');
+    const mapPopup = document.getElementById('mapPopup');
+    const dropmarkers = document.getElementById('dropmarkers');
+    reciepContent.style.transition =
+      'width 0.3s ease-in, height 0.3s ease-in, margin-right 0.3s ease-in';
+    reciepContent.style.width = '3.5rem';
+    reciepContent.style.height = 'calc(100vh - 3.5rem)';
+    reciepContent.style.marginRight = '0.5rem';
+    wholemodalReciep.classList.add('d-none');
+    mapPopup.classList.remove('d-none');
+    mapPopup.style.height = 'calc(100vh - 3.5rem)';
+    mapPopup.classList.add('w-100');
+    setTimeout(() => {
+      initMap(storesForMap);
+      dropmarkers.classList.remove('d-none');
+    }, 300);
+    dropmarkers.addEventListener('click', ev => {
+      ev.preventDefault();
+      mapPopup.classList.remove('w-100');
+      mapPopup.classList.add('d-none');
+      reciepContent.style.width = '100%';
+      setTimeout(() => {
+        mapPopup.style.height = '0px';
+        reciepContent.style.height = '100%';
+        wholemodalReciep.classList.remove('d-none');
+        reciepContent.style.marginRight = '0px';
+        dropmarkers.classList.add('d-none');
+      }, 300);
+    });
+  });
+}
 function setupIngredients(whatToBuy) {
   const allIngs = document.getElementById('allIngs');
   allIngs.querySelectorAll('span').forEach(ing => {
-    console.log(whatToBuy);
-    console.log(ing.innerHTML);
     if (whatToBuy.includes(ing.innerHTML.toLowerCase())) {
       ing.parentElement.classList.add('list-group-item-danger');
     }
@@ -77,7 +125,9 @@ function popup() {
       $(() => {
         $('[data-toggle="tooltip"]').tooltip();
       });
-
+      const mapButton = document.getElementById('openMap');
+      mapButton.setAttribute('disabled', 'true');
+      mapButton.innerHTML = 'Searching for prices...';
       $('#myModal').modal('show');
       $('#myModal').on('hidden.bs.modal', () => {
         window.history.back();
@@ -88,10 +138,51 @@ function popup() {
       const whatToBuy = [...document.querySelectorAll('.ingredientsSingle span')]
         .map(e => e.innerHTML.toLowerCase())
         .filter(el => !currentLocalStorage.includes(el));
-
-      setupStores(await loadStoresAndPries(whatToBuy));
+      const storesAndPrices = await loadStoresAndPries(whatToBuy);
+      setupStores(storesAndPrices);
       setupIngredients(whatToBuy);
+      const storesForMap = storesAndPrices.map(el => {
+        switch (el.name) {
+          case 'Сільпо':
+            return {
+              name: el.name,
+              name2: 'Silpo',
+              name3: 'Сильпо',
+              price: el.price
+            };
+          case 'АТБ':
+            return {
+              name: el.name,
+              name2: 'Atb',
+              name3: 'АТБ',
+              price: el.price
+            };
+
+          case 'Novus':
+            return {
+              name: 'Новус',
+              name2: el.name,
+              name3: 'Новус',
+              price: el.price
+            };
+
+          case 'Велика Кишеня':
+            return {
+              name: el.name,
+              name2: 'Velyka Kyshenya',
+              name3: el.name,
+              price: el.price
+            };
+
+          default:
+            return {};
+        }
+      });
+      mapButton.removeAttribute('disabled');
+      mapButton.innerHTML = 'See on Map';
+
+      initializeMap(mapButton, storesForMap);
     };
   });
 }
-export { popup, loadStoresAndPries, setupStores, setupIngredients };
+export { popup, loadStoresAndPries, setupStores, setupIngredients, initializeMap };
