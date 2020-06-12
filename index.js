6b3c3c86cdf5f095d252c9aaaa906fcc6e8ea32b
metamodern <shabanovbunya@gmail.com>
2020-06-12 19:07:57 +0300

    add no-commenting for ssl

--- index.js
+++ index.js
@@ -28,13 +28,15 @@ render(app, {
 });
 
 app
-  // .use(async (ctx, next) => {
-  //   if (ctx.get('X-Forwarded-Proto') !== 'https' && ctx.get('X-Forwarded-Port') !== '443') {
-  //     ctx.redirect(`https://${ctx.request.header.host}${ctx.url}`);
-  //   } else {
-  //     await next();
-  //   }
-  // })
+  .use(async (ctx, next) => {
+    if (ctx.hostname !== 'localhost') {
+      if (ctx.get('X-Forwarded-Proto') !== 'https' && ctx.get('X-Forwarded-Port') !== '443') {
+        ctx.redirect(`https://${ctx.request.header.host}${ctx.url}`);
+      }
+    } else {
+      await next();
+    }
+  })
   .use(serve('public'))
   .use(koaBody())
   .use(middleware)
